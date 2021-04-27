9c079c99caed270557647ef34bf4fb10b6d2e08a
김대일 <os1227os@naver.com>
2021-04-27 23:13:06 +0900

    4/27

---
+++ 문자열/1316.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void){
+  
+  int num,i,cnt;
+  scanf("%d",&num);
+  
+  char arr[100];
+
+  for(i=0;i<num;i++){
+    scanf("%s", arr);
+    
+  }
+
+    
+
+  return 0;
+}
+
